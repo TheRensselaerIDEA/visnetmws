877b9b897a97a44ccffd9a7e4243ac8346a3b446
taraskami <taras.kami@outlook.com>
2019-09-24 21:12:04 -0400

    sept 24 cleanup

--- app.R
+++ app.R
@@ -37,7 +37,6 @@ if (!require("htmlwidgets")) {
 }
 
 
-
 ui_win <- list()
 
 # first we add what we want to see in the controller to the list
@@ -52,17 +51,23 @@ ui_win[["Floor"]] <- fillPage(
 )
 
 ui_win[["Wall"]] <- fluidPage(
+  # setBackgroundColor(color="black"),
   titlePanel("visnetmws"),
-  sidebarLayout(position = "left",
-    sidebarPanel(
-      textOutput("node_name"),
-      imageOutput("node_pic")
-      
-    ),
-    mainPanel(
-      textOutput("node_desc")
-    )
-  )
+  # imageOutput("test_Blank.jpg"),
+  uiOutput("members_output")
+  
+  
+  
+  # sidebarLayout(position = "left",
+  #   sidebarPanel(
+  #     textOutput("node_name"),
+  #     imageOutput("node_pic")
+  #     
+  #   ),
+  #   mainPanel(
+  #     textOutput("node_desc"),
+  #     uiOutput("group_display")
+  #   )
 )
 
 
@@ -77,40 +82,115 @@ serv_calc[[1]] <- function(input,calc){
 
 serv_out <- list()
 
-serv_out[["node_desc"]] <- function (input, calc) {
-  renderText({
-    if(!is.null(input$current_node_id)){
-      if (typeof(input$current_node_id) == "character") {
-        paste("ID:", input$current_node_id)
-      }
-        name <- nodes[input$current_node_id+1, 5]
-        paste("ID:", input$current_node_id, "\n Name:", name)
-    } else {
-      "Node not selected"
+# serv_out[["node_desc"]] <- function (input, calc) {
+#   renderText({
+#     if(!is.null(input$current_node_id)){
+#       if (typeof(input$current_node_id) == "character") {
+#         paste("ID:", input$current_node_id)
+#       }
+#         name <- nodes[input$current_node_id+1, 5]
+#         paste("ID:", input$current_node_id, "\n Name:", name)
+#     } else {
+#       "Node not selected"
+#     }
+#   })
+# }
+# 
+# # add a image on the wall
+# 
+# serv_out[["node_pic"]] <- function (input, calc) {
+#  renderImage({
+#    if(!is.null(input$current_node_id)){
+#      
+#      node_id <<- input$current_node_id
+#      
+#      if (typeof(node_id) == "character") {
+#        list(src = "Pictures/Blank.jpg", alt = "No photo available.", width = "400px", height = "400px")
+#      } else {
+#        tmp <<- as.character(nodes[node_id+1, 6])
+#        
+#        list(src = tmp, alt = nodes[input$current_node_id+1, 5], width = "400px", height = "400px")
+#      }
+#    }
+#  }, deleteFile = FALSE)
+# }
+
+# member_images <- list()
+# 
+# 
+# render_member_images <- function(input, group) {
+#     
+#       #Getting member names in group
+#       group <<- strsplit(node_id, ":")[[1]][2]
+#       group_members <<- as.list(nodes[nodes$group == group, 5])
+#       group_members <<- group_members[["title"]]
+#       
+#       i <- 0
+#       for (member in group_members) {
+#         member_images[[paste0("member", i )]] <- renderImage({list(
+#           src = "www/Blank.jpg"
+#         )}, deleteFile = FALSE)
+#         i <- i + 1
+#       }
+#       
+# }
+
+pic_names <- list.files(path = "./www")
+
+for (pic_name in pic_names) {
+  
+  pic_name_fn <- paste("function(input, calc) {
+                        renderImage({list(src=\"www/",
+                        pic_name,
+                        "\")}, deleteFile = FALSE)}",
+                        sep="")
+  
+  cat(paste("this is wack but", pic_name_fn, "\n"), stderr())
+  
+  
+  
+  serv_out[[pic_name]] <- eval(parse(text = pic_name_fn))
+  
+  # serv_out[[pic_name]] <- function(input, calc) {
+  #   
+  #   print(paste0("pic name is ", pic_name), stderr())
+  #   
+  #   tmp <- paste("www/", pic_name, sep="")
+  #   
+  #   renderImage(
+  #     {list(src=tmp)},
+  #     deleteFile = FALSE
+  #   )
+  #   
+  # }
+}
+
+
+
+
+
+serv_out[["test_Blank.jpg"]] <- function(input, calc) {
+  renderImage(
+    {list(src="www/Blank.jpg")},
+    deleteFile = FALSE
+  )
+}
+
+serv_out[["members_output"]] <- function(input, calc) {
+  
+  renderUI({
+    node_id <<- input$current_node_id
+    if (!is.null(node_id)) {
+      
+        # If node selected is a group:
+        if (typeof(node_id) == "character") {
+          # render_member_images(input,node_id)
+        }
+        
+        imageOutput("Bosseut.jpg")
     }
   })
 }
-
-# add a image on the wall
-
-serv_out[["node_pic"]] <- function (input, calc) {
- renderImage({
-   if(!is.null(input$current_node_id)){
-     
-     node_id <<- input$current_node_id
-     
-     if (typeof(node_id) == "character") {
-       list(src = "Pictures/Blank.jpg", alt = "No photo available.", width = "400px", height = "400px")
-     } else {
-       tmp <<- as.character(nodes[node_id+1, 6])
-       
-       list(src = tmp, alt = nodes[input$current_node_id+1, 5], width = "400px", height = "400px")
-     }
-
-   }
- }, deleteFile = FALSE)
-}
-
 
 # Here we render our network
 # note the name is the same as the outputid
